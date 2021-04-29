9ec63507183314c1220b282db64b162a600a9979
Keven Jude Anténor <kevenjudeantenor@Kevens-MacBook-Air.local>
2021-04-29 17:42:54 -0400

    Voilà

--- src/ListeIndex.java
+++ src/ListeIndex.java
@@ -1,30 +1,68 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListeIndex<E extends Comparable< E > > {
+
+    private List<ListeMilieu<E>> milieux = new ArrayList<>();
+
+    /**
+     * Vérifie si la valeur est dans la ListeIndex
+     * @param valeur
+     * @return
+     */
     public boolean contient( E valeur ) {
-        return false;
+        boolean trouver = false;
+
+        for (ListeMilieu<E> mil: milieux) {
+            if (mil.contient(valeur)) {
+                trouver = true;
+            }
+        }
+
+        return trouver;
     }
-    /*
-        Cette methode sert à savoir si le résultat est null.
+    /**
+        Retourne la ListeMilieu à l'index i.
+        @param i : index à retourner
      */
     public ListeMilieu<E> get( int i ) {
-        return null;
+        return milieux.get(i);
     }
 
+    /**
+     * Insère la valeur dans la bonne ListeMilieu (selon le minima et maxima de chaque
+     * liste).
+     * @param valeur
+     */
     public void inserer( E valeur ) {
 
     }
 
+    /**
+     * Retourne le nombre de ListMilieu dans la ListeIndex.
+     * @return
+     */
     public int nbrListe() {
-        return 0;
+        return milieux.size();
     }
 
-    /*
-        Cette methode sert à supprimer les valeurs de trop.
+    /**
+     * Supprime la valeur si elle existe.
+     * @param valeur : la valeur à supprimer
      */
     public void supprimer( E valeur ) {
 
     }
 
+    /**
+     * Retourne le nombre d'éléments dans la ListeIndex.
+     * @return
+     */
     public int taille() {
-        return 0;
+        int taille_totale = 0;
+        for (ListeMilieu<E> mil : milieux) {
+            taille_totale += mil.taille();
+        }
+        return taille_totale;
     }
 }
