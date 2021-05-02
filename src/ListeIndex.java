660a44eae424c4f7b980f3efd8d89d366abb895e
Keven Jude Anténor <kevenjudeantenor@Kevens-MacBook-Air.local>
2021-05-02 00:26:33 -0400

    J'ai travaillé dans listeIndex et ça me donne des erreurs

--- src/ListeIndex.java
+++ src/ListeIndex.java
@@ -3,7 +3,13 @@ import java.util.List;
 
 public class ListeIndex<E extends Comparable< E > > {
 
-    private List<ListeMilieu<E>> milieux = new ArrayList<>();
+    private List<ListeMilieu<E>> milieux ;
+    private int nbrListe;
+
+    public ListeIndex() {
+        milieux = new ArrayList<>(10);
+        nbrListe = 0;
+    }
 
     /**
      * Vérifie si la valeur est dans la ListeIndex
@@ -35,7 +41,37 @@ public class ListeIndex<E extends Comparable< E > > {
      * @param valeur
      */
     public void inserer( E valeur ) {
+        boolean estInseree = false;
+        int i;
+        if (nbrListe == 0){
+            ListeMilieu<E> ajou = new ListeMilieu<>();
+            ajou.inserer(valeur);
+            milieux.add(ajou);
+        } else {
+            for (i = 0; i < milieux.size() && milieux.get(i) != null
+                    && milieux.get(i+1) != null
+                    && !estInseree; i++) {
+                if (i != milieux.size() - 1
+                        && milieux.get(i).minima().compareTo(valeur) <= 0
+                        && milieux.get(i+1).minima().compareTo(valeur) > 0) {
+                    milieux.get(i).inserer(valeur);
 
+
+                    estInseree = true;
+                } else if (!estInseree && i == milieux.size() - 1
+                        && milieux.get(i).minima().compareTo(valeur) <= 0) {
+                    milieux.get(i).inserer(valeur);
+
+                }
+            }
+
+            if (estInseree && !(milieux.get(i).taille() <= 2 * nbrListe)) {
+                milieux.add(milieux.get(i).diviser());
+            }
+        }
+
+
+        //valeur ---E; creer --listMilieu; ajoute lui. si il existe deja, il faut verifier.
     }
 
     /**
@@ -43,7 +79,7 @@ public class ListeIndex<E extends Comparable< E > > {
      * @return
      */
     public int nbrListe() {
-        return milieux.size();
+        return nbrListe;
     }
 
     /**
@@ -51,6 +87,17 @@ public class ListeIndex<E extends Comparable< E > > {
      * @param valeur : la valeur à supprimer
      */
     public void supprimer( E valeur ) {
+//valeur (E); milieux (ListMilieux), arraylist; E --- ListMilieux
+
+
+       if(nbrListe > 0) {
+            for (ListeMilieu<E> mil : milieux) {
+                if (mil.contient(valeur)) {
+                    mil.supprimer(valeur);
+                }
+            }
+        }
+
 
     }
 
